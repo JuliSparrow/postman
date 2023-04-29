b4f84f01aadefaf5a322888d7103e25267247a21
QA <feverx@inbox.ru>
2023-04-29 18:22:04 +0300

    Исправлено название тестового метода и тело запроса

--- src/test/java/ru/netology/postman/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/postman/MobileBankApiTestV1.java
@@ -3,22 +3,23 @@ package ru.netology.postman;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldReturnEcho() {
         // Given - When - Then
         // Предусловия
         given()
             .baseUri("https://postman-echo.com")
-            .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+            .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
         // Выполняемые действия
         .when()
             .post("/post")
         // Проверки
         .then()
             .statusCode(200)
-//            .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("Hello"))
         ;
     }
 }
